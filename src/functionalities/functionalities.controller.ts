6ceb39954a8d6b1af347e116680513959cc2e6c4
Yousef Shaban <yousefshaban.eg@gmail.com>
2023-03-13 16:29:01 +0200

    Get Functionalities with it's related platform prices.

--- src/functionalities/functionalities.controller.ts
+++ src/functionalities/functionalities.controller.ts
@@ -1,11 +1,15 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Body, Controller, Get, Res, ValidationPipe } from '@nestjs/common';
+import { Functionality } from './dto/functionality.dto';
 import { FunctionalitiesService } from './functionalities.service';
 
 @Controller('functionalities')
 export class FunctionalitiesController {
   constructor(private functionalitiesService: FunctionalitiesService) {}
   @Get()
-  async getFunctionalities(@Res() res) {
-    return this.functionalitiesService.getFunctionalities(res);
+  async getFunctionalities(
+    @Res() res,
+    @Body(new ValidationPipe({ transform: true })) body: Functionality,
+  ) {
+    return this.functionalitiesService.getFunctionalities(res, body);
   }
 }
--- src/functionalities/functionalities.service.ts
+++ src/functionalities/functionalities.service.ts
@@ -8,7 +8,76 @@ export class FunctionalitiesService {
     private responseService: ResponseService,
     private prisma: PrismaService,
   ) {}
-  async getFunctionalities(res) {
-    const functionalities=await this.prisma.functionalities.findMany()
+
+  async getFunctionalities(res, body) {
+    const { orderId } = body;
+    const platformIdsHourPrice = {};
+    const platforms = await this.prisma.orders.findFirst({
+      where: {
+        id: orderId,
+      },
+      select: {
+        PlatformOrders: {
+          select: {
+            Platform: {
+              select: {
+                id: true,
+                name: true,
+                subtitle: true,
+                hourPrice: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    platforms.PlatformOrders.forEach(
+      (p) => (platformIdsHourPrice[p.Platform.id] = p.Platform.hourPrice),
+    );
+
+    const functionalitiesFromDB = await this.prisma.functionalities.findMany({
+      select: {
+        id: true,
+        name: true,
+        subtitle: true,
+        FunctionalitiesRequirements: {
+          include: {
+            Requirement: true,
+          },
+        },
+        PlatformsFunctionalities: {
+          where: {
+            platformId: {
+              in: Object.keys(platformIdsHourPrice),
+            },
+          },
+        },
+      },
+    });
+
+    const functionalitiesWithRequirements = [];
+
+    functionalitiesFromDB.map((f) => {
+      const functionality = { ...f, requirements: [] };
+      let hours = 0;
+      let price = 0;
+      f.PlatformsFunctionalities.forEach((pf) => {
+        hours += pf.hours;
+        price += platformIdsHourPrice[pf.platformId] * pf.hours;
+      });
+      functionality.requirements =
+        functionality.FunctionalitiesRequirements.map((e) => e.Requirement);
+      delete functionality.FunctionalitiesRequirements;
+      delete functionality.PlatformsFunctionalities;
+
+      functionalitiesWithRequirements.push({ ...functionality, hours, price });
+    });
+
+    return this.responseService.success(
+      res,
+      'Functionalities',
+      functionalitiesWithRequirements,
+    );
   }
 }
